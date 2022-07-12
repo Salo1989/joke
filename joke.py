070fe777ba2b48a364d3e019b1f89c8e29a4d0ec
Salomon Elguera <106698702+Salo1989@users.noreply.github.com>
2022-07-12 21:08:48 +0300

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,15 @@
+#program will ask if you want to hear joke
+#yes=tells joke
+#no=gets offended
+print("want to hear a joke?")
+print("answer yes or no")
+user_import=input("Enter yes or no:")
+answer1="yes"
+answer2="no"
+
+if user_import==answer1:
+    print(" Why do we tell actors to break a leg")
+    print(" Because every show has a cast")
+
+else:
+    print("Hey!!!!! No reason to be mean")
